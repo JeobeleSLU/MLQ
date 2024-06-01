74355ffe63a34016a5229dac5df4fbe0a3c09269
Jeobele <jeobelecedeno121304@gmail.com>
2024-06-01 15:56:30 +0800

    refactored npps

--- src/Core.java
+++ src/Core.java
@@ -8,6 +8,11 @@ public class Core {
         get the processes
         run the highest priority process
         based on the algorithm
+        example if a process have arrive
+        it should run the highest one first
+        rounrobin.run something
+        then check the lower priority
+
          */
 
 
